695c6b93364158c60554109b96c7449818e1ccf5
frankuman <oliverbolin97@gmail.com>
2022-01-24 17:42:16 +0100

    Compiling stuff

---
+++ AV 0.1/build/CMakeFiles/AV.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AV.dir/AV.cpp.o"
+  "libAV.pdb"
+  "libAV.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AV.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
