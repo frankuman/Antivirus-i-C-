3be69b5e4b768a4a32331ff1c77723d1d6c01ebc
frankuman <oliverbolin97@gmail.com>
2022-01-24 15:44:42 +0100

    Create AV_1.0.cpp

---
+++ AV_1.0.cpp
@@ -0,0 +1,11 @@
+//Gjort av Oliver Bölin & Oskar Hansson
+//Kurskod 1620
+//Startdatum 24/01/2202
+
+using namespace std;
+#include <iostream>
+
+int main(){
+    cout << "Projekt 1, antivirus" << endl;
+
+};
